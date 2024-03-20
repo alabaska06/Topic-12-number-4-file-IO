64883ce932e8eb00fb92808f5341b6a418d03911
alabaska06 <smitalan240@gmail.com>
2024-03-20 11:00:25 -0400

    Add project files.

---
+++ EventScore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_12_number_4_file_IO
+{
+    internal class EventScore
+    {
+        public string Name { get; set; }
+        public string Event {  get; set; }
+        List<double> Scores { get; set; }
+
+        public EventScore(string name, string eventName, List<double> scores)
+        {
+            Name = name;
+            Event = eventName;
+            Scores = scores;
+        }
+
+        public double GetTotalScore()
+        {
+            return Scores.Sum();
+        }
+
+        public double GetAverage()
+        {
+            return Scores.Average();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Event}: Total Score: {GetTotalScore()}, Average Score: {GetAverage()}"; 
+        }
+        public int CompareTo(EventScore other)
+        {
+            return GetTotalScore().CompareTo(other.GetTotalScore());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Serialization;
+
+namespace Topic_12_number_4_file_IO
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<EventScore> eventScores = ReadEventScoresFromFile(@"results.txt");
+
+            int choice;
+            do
+            {
+                Console.WriteLine("Menu:\n");
+                Console.WriteLine("1. Print Scores");
+                Console.WriteLine("2. Highest Score");
+                Console.WriteLine("3. Lowest Score");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("\nEnter your choice:\n");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Scores:");
+                        Console.WriteLine(eventScores);
+                        break;
+                    case 2:
+                        Console.WriteLine("Highest Scores:");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+
+
+        }
+
+    }
+}
